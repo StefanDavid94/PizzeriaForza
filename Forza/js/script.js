8442b279fc82b5932547091d5dd63f4c82c0539c
StefanDavid <59165629+StefanDavid94@users.noreply.github.com>
2021-01-30 19:44:50 +0100

    Add files via upload

---
+++ Forza/js/script.js
@@ -0,0 +1,246 @@
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+
+//console.log(links.classList);
+//console.log(links.classList.contains("random"));
+//console.log(links.classList.contains("links"));
+
+//if(links.classList.contains('show-links')){
+
+//links.classList.remove("show-links");}
+
+//else {
+
+//links.classList.add("show-links");}
+
+links.classList.toggle('show-links');
+
+
+
+
+});
+
+
+//FIXED NAVBAR
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("navBar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+
+/* ***** Slideanim  ***** */
+ $(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+}); 
+
+
+
+
+
+
+
+
+
+  
+// numbers
+
+   jQuery(document).ready(function($) {
+            $(".num").counterUp({
+                delay: 10,
+                time: 1000
+            });
+        });
+
+// testimonials
+
+// vars
+'use strict'
+var	testim = document.getElementById("testim"),
+		testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+		touchStartPos,
+		touchEndPos,
+		touchPosDiff,
+		ignoreTouch = 30;
+;
+
+window.onload = function() {
+
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length-1;
+        }
+
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");            
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+    
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function() {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+
+    testimLeftArrow.addEventListener("click", function() {
+        playSlide(currentSlide -= 1);
+    })
+
+    testimRightArrow.addEventListener("click", function() {
+        playSlide(currentSlide += 1);
+    })    
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function() {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+
+    playSlide(currentSlide);
+
+    // keyboard shortcuts
+    document.addEventListener("keyup", function(e) {
+        switch (e.keyCode) {
+            case 37:
+                testimLeftArrow.click();
+                break;
+                
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            default:
+                break;
+        }
+    })
+		
+		testim.addEventListener("touchstart", function(e) {
+				touchStartPos = e.changedTouches[0].clientX;
+		})
+	
+		testim.addEventListener("touchend", function(e) {
+				touchEndPos = e.changedTouches[0].clientX;
+			
+				touchPosDiff = touchStartPos - touchEndPos;
+			
+				console.log(touchPosDiff);
+				console.log(touchStartPos);	
+				console.log(touchEndPos);	
+
+			
+				if (touchPosDiff > 0 + ignoreTouch) {
+						testimLeftArrow.click();
+				} else if (touchPosDiff < 0 - ignoreTouch) {
+						testimRightArrow.click();
+				} else {
+					return;
+				}
+			
+		})
+}
+
+
+
+
+
+//Scroll reveal
+
+
+window.sr = ScrollReveal();
+
+
+sr.reveal('.animate-left',{
+origin:'left',
+duration:1000,
+distance:'25rem',
+delay:300
+
+
+
+});
+
+
+sr.reveal('.animate-right',{
+origin:'right',
+duration:1000,
+distance:'25rem',
+delay:600
+
+
+
+});
+
+sr.reveal('.animate-top',{
+origin:'top',
+duration:1000,
+distance:'25rem',
+delay:600
+
+
+
+});
+
+
+sr.reveal('.animate-bottom',{
+origin:'bottom',
+duration:1000,
+distance:'25rem',
+delay:600
+
+
+
+});
+
+// NavFixedTop
+
+
+
